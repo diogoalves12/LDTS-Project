8facb220f006e70d7c142cd3aa96ec75671963cc
Diogo Alves <diogoalves@MacBook-Air-de-Diogo-1.local>
2024-11-22 15:29:52 +0000

    Set up screen drawing and game loop

--- src/main/java/GameScreen.java
+++ src/main/java/GameScreen.java
@@ -1,8 +1,12 @@
+import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.Terminal;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.graphics.TextGraphics;
+
+
 import java.io.IOException;
 
 public class GameScreen {
@@ -11,13 +15,31 @@ public class GameScreen {
 
     public GameScreen() throws IOException {
         TerminalSize terminalSize = new TerminalSize(80, 80);
+
         DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
         Terminal terminal = terminalFactory.createTerminal();
+
         screen = new TerminalScreen(terminal);
 
         screen.setCursorPosition(null); // remover depois, vamos precisar do cursor
         screen.startScreen();
         screen.doResizeIfNecessary();
     }
+
+    private void drawScreen() throws IOException {
+        screen.clear();
+        screen.refresh();
+    }
+
+    private void processKey(){
+        // To do
+    }
+
+    public void run() throws IOException {
+        while(true) {
+            drawScreen();
+        }
+    }
+
 
 }
