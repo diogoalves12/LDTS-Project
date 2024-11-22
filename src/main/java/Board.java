e300a309eafd9190bb6e1b84db2c3283b88d0f4a
Diogo Alves <diogoalves@MacBook-Air-de-Diogo-1.local>
2024-11-22 17:19:03 +0000

    Set up the base of the CreateBoard method

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -8,13 +8,23 @@ public class Board {
         this.rows = rows;
         this.cols = cols;
         this.mines = mines;
-        board = new Cell[rows][cols];
         CreateBoard();
     }
 
     private void CreateBoard() {
+        board = new Cell[rows][cols];
+
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                board[row][col] = new Cell();
+            }
+        }
+
         // --> completar
+        // bombas
+        // bombas adjacentes
     }
+
 
 
 }
