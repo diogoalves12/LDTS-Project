0c1da52edfd8e6c36b70170801718630cfb5ed3b
Diogo Alves <diogoalves@MacBook-Air-de-Diogo-1.local>
2024-11-22 13:01:32 +0000

    Set up constructor and 2D array

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,2 +1,21 @@
 public class Board {
+    private Cell[][] board;
+    private int rows;
+    private int cols;
+    private int mines;
+
+    public Board(int rows, int cols, int mines) {
+        this.rows = rows;
+        this.cols = cols;
+        this.mines = mines;
+        board = new Cell[rows][cols];
+        CreateBoard();
+    }
+
+    private void CreateBoard() {
+        // --> completar
+    }
+
+
 }
+
