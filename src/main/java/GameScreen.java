8dcc6a43db9fcdcbd80b583179cbca212279cd2a
Diogo Alves <diogoalves@MacBook-Air-de-Diogo-1.local>
2024-11-22 14:57:32 +0000

    Set up intial constructor

--- src/main/java/GameScreen.java
+++ src/main/java/GameScreen.java
@@ -1,3 +1,23 @@
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.Terminal;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.TerminalSize;
+import java.io.IOException;
+
 public class GameScreen {
+
+    private static Screen screen;
+
+    public GameScreen() throws IOException {
+        TerminalSize terminalSize = new TerminalSize(80, 80);
+        DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
+        Terminal terminal = terminalFactory.createTerminal();
+        screen = new TerminalScreen(terminal);
+
+        screen.setCursorPosition(null); // remover depois, vamos precisar do cursor
+        screen.startScreen();
+        screen.doResizeIfNecessary();
+    }
 
 }
