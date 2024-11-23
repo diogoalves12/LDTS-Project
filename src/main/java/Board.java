867db167c60dc9ab2f68d30e7a1a3bb6437418b0
Diogo Alves <diogoalves@MacBook-Air-de-Diogo-1.local>
2024-11-23 02:14:07 +0000

    improve the board initializer method and added the place mines method

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,31 +1,46 @@
+import java.util.Random;
+
 public class Board {
     private Cell[][] board;
     private int rows;
     private int cols;
-    private int mines;
+    private int numMines;
 
     public Board(int rows, int cols, int mines) {
         this.rows = rows;
         this.cols = cols;
-        this.mines = mines;
-        CreateBoard();
+        this.numMines = mines;
+        board = new Cell[rows][cols];
+        initializeBoard();
     }
 
-    private void CreateBoard() {
-        board = new Cell[rows][cols];
+    private void initializeBoard() {
 
         for (int row = 0; row < rows; row++) {
             for (int col = 0; col < cols; col++) {
                 board[row][col] = new Cell();
             }
-        }
 
-        // --> completar
-        // bombas
-        // bombas adjacentes
+            placeMines();
+            NumAjacentMines();
+        }
     }
 
+    private void placeMines () {
+        Random random = new Random();
+        int minesPlaced = 0;
 
+        while (minesPlaced < numMines) {
+            int row = random.nextInt(rows);
+            int col = random.nextInt(cols);
 
+            if (!cells[row][col].hasMine()) {
+
+            }
+        }
+    }
+
+    private void NumAjacentMines(){
+
+    }
 }
-
