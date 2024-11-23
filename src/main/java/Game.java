fe39bb77f22058fa63ecee1315dfdbef91514fa8
Diogo Alves <diogoalves@MacBook-Air-de-Diogo-1.local>
2024-11-23 22:06:51 +0000

    Added Game atributes

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,2 +1,16 @@
+import com.googlecode.lanterna.screen.Screen;
+
+import java.io.IOException;
+
 public class Game {
+    private Board board;
+    private GameScreen gameScreen;
+    private boolean gameOver;
+
+    public Game(int rows, int cols, int numMines) throws IOException {
+        this.board = new Board(rows, cols, numMines);
+        this.gameScreen = new GameScreen();
+        this.gameOver = false;
+    }
+
 }
