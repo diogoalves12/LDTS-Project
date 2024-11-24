790f8352c607e8e8bbac47296a19365e1e5ba042
Diogo Alves <diogoalves@MacBook-Air-de-Diogo-1.local>
2024-11-24 16:27:02 +0000

    Added a test for default cell state

--- src/test/java/CellTest.java
+++ src/test/java/CellTest.java
@@ -1,2 +1,20 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class CellTest {
+    private Cell cell;
+
+    @BeforeEach
+    public void setUp() {
+        cell = new Cell();
+    }
+
+    @Test
+    public void testDefaultCell() {
+        Assertions.assertFalse(cell.hasMine());
+        Assertions.assertFalse(cell.isFlagged());
+        Assertions.assertFalse(cell.isRevealed());
+        Assertions.assertEquals(0, cell.getAdjacentMines());
+    }
 }
