6334df44c85de958e98e41e1fa2a53593a0082ed
Diogo Alves <diogoalves@MacBook-Air-de-Diogo-1.local>
2024-11-24 16:20:55 +0000

    Added a method that tests the mines placemet

--- src/test/java/BoardTest.java
+++ src/test/java/BoardTest.java
@@ -3,13 +3,33 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class BoardTest {
+    private Board board;
+
+    @BeforeEach
+    public void setUp() {
+        board = new Board(10,10,10);
+    }
 
     @Test
     public void testBoardInitialization() {
-        Board board = new Board(10,10,10);
+        Assertions.assertEquals(10, board.getRows());
+        Assertions.assertEquals(10, board.getCols());
+        Assertions.assertEquals(10, board.getNumMines());
+    }
 
-        Assertions.assertEquals(10,board.getRows());
-        Assertions.assertEquals(10,board.getCols());
+    @Test
+    public void testMinePlacement(){
+        int expectedMines = board.getNumMines();
+        int mineCount = 0;
+
+        for(int row = 0; row < board.getRows(); row++){
+            for(int col = 0; col < board.getCols(); col++){
+                if(board.getCell(row,col).hasMine()){
+                    mineCount++;
+                }
+            }
+        }
+        Assertions.assertEquals(expectedMines, mineCount);
     }
 
 }
