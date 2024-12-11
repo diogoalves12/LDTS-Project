89014e8acb86774f1a4c6825a535eb1201e3d470
Diogo Alves <diogocalves26@gmail.com>
2024-12-11 20:01:17 +0000

    Created contructor and a method

--- src/main/java/model/MineCell.java
+++ src/main/java/model/MineCell.java
@@ -1,4 +1,14 @@
 package model;
 
-public class MineCell {
+public class MineCell extends Cell {
+
+    public MineCell(int row, int col) {
+        super(row, col);
+    }
+
+    @Override
+    public boolean hasMine(){
+        return true;
+    }
+
 }
