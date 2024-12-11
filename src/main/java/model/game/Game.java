71394c042c06bda5d9258ec345fd3a3cb581a83e
Diogo Alves <diogocalves26@gmail.com>
2024-12-11 23:28:30 +0000

    Added some game logic

--- src/main/java/model/game/Game.java
+++ src/main/java/model/game/Game.java
@@ -1,12 +1,49 @@
-package model;
+package model.game;
+
+import model.Setup;
 
 public class Game {
 
     private Board board;
+    Setup setup;
 
-    public Game(Board board) {
-        this.board = Board.getInstance();
+    public Game(Setup setup) {
+        this.setup = setup;
+        int freq;
+        switch (setup.getDifficulty()){
+            case EASY:
+                freq = 8;
+                break;
+            case NORMAL:
+                freq = 6;
+                break;
+            case HARD:
+                freq = 5;
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid difficulty");
+        }
+        this.board = new BoardBuilder(setup.getBoardRows(),setup.getBoardRows()).buildBoard(freq);
+    }
+
+    public Board getBoard() {
+        return board;
+    }
+
+    public Setup getSetup() {
+        return setup;
+    }
+
+    public int getRows(){
+        return setup.getBoardRows();
+    }
+
+    public int getColumns(){
+        return setup.getBoardRows();
     }
 
 
+
+
 }
+
