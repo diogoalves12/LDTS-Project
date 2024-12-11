ff27dbdfacc5775526cef8c2a5b2a1e6e6bcf98a
Diogo Alves <diogocalves26@gmail.com>
2024-12-11 20:06:11 +0000

    Created contructor and some  methods

--- src/main/java/model/NormalCell.java
+++ src/main/java/model/NormalCell.java
@@ -1,6 +1,24 @@
 package model;
 
 public class NormalCell extends Cell {
+    private int adjacentMines;
 
+    public NormalCell(int x, int y) {
+        super(x, y);
+        adjacentMines = 0;
+    }
+
+    public void setAdjacentMines(int adjacentMines) {
+        this.adjacentMines = adjacentMines;
+    }
+
+    public int getAdjacentMines() {
+        return adjacentMines;
+    }
+
+    @Override
+    public boolean hasMine() {
+        return false;
+    }
 
 }
