f795fa6a9e654d3f3bdbddca11d8102beb1e8f09
Diogo Alves <diogocalves26@gmail.com>
2024-12-12 22:44:19 +0000

    Added mehtod to create the board viewer

--- src/main/java/view/ViewerFactory.java
+++ src/main/java/view/ViewerFactory.java
@@ -1,4 +1,17 @@
 package view;
 
+import com.googlecode.lanterna.screen.Screen;
+import model.game.Board;
+import view.game.BoardViewer;
+
 public class ViewerFactory {
+    private final Screen screen;
+
+    public ViewerFactory(Screen screen) {
+        this.screen = screen;
+    }
+
+    public static Viewer<Board> createBoardViewer(Board board, Screen screen) {
+        return new BoardViewer(board, screen);
+    }
 }
