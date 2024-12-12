d5ef2f43d9fed690ac2d2b9b7befd182b91a2b20
Diogo Alves <diogocalves26@gmail.com>
2024-12-12 23:21:52 +0000

    Created the constructur and methods needed

--- src/main/java/model/menu/MenuModel.java
+++ src/main/java/model/menu/MenuModel.java
@@ -1,4 +1,35 @@
 package model.menu;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MenuModel {
+    private final List<String> options;
+    private int selected;
+
+    public MenuModel(List<String> options) {
+        this.options =  new ArrayList<>();
+        this.options.add("Play");
+        this.options.add("Help");
+        this.options.add("Quit");
+        this.selected = 0;
+    }
+
+    public List<String> getOptions() {
+        return options;
+    }
+
+    public boolean isSelected(int index) {
+        return index == selected;
+    }
+
+    public void select(int index) {
+        if(index >= 0 && index < options.size()) {
+            selected = index;
+        }
+    }
+
+    public int getSelected() {
+        return selected;
+    }
 }
