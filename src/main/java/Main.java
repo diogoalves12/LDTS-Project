64f75de7f69429346a2fc576f7bd4611d6165aaa
Diogo Alves <diogocalves26@gmail.com>
2024-12-12 20:59:59 +0000

    updated main class to test the new classes and methods

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,11 @@
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+import model.Setup;
+import model.game.Board;
+import model.game.BoardBuilder;
 import view.game.GameViewer;
 
 import java.io.IOException;
@@ -5,11 +13,37 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args){
         try{
-            GameViewer gameScreen = new GameViewer();
-            gameScreen.run();
+            Setup setup = new Setup(10,10, Setup.Difficulty.NORMAL);
+
+            Board.initialize(setup.getBoardRows(), setup.getBoardCols());
+            Board board = Board.getInstance();
+
+            BoardBuilder builder = new BoardBuilder(null);
+            builder.buildBoard(6);
+
+            TerminalSize terminalSize = new TerminalSize(board.getCols() * 2 + 2, board.getRows() + 2);
+            DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
+            Terminal terminal = terminalFactory.createTerminal();
+            Screen screen = new TerminalScreen(terminal);
+
+            screen.setCursorPosition(null);
+            screen.startScreen();
+            screen.doResizeIfNecessary();
+
+            GameViewer viewer = new GameViewer(board, screen);
+
+            while (true) {
+                viewer.draw();
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
         } catch (IOException e) {
             e.printStackTrace();
-            System.exit(1);
+
         }
 
 
