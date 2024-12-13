8272f0d63a7451dfc66aeb7a821f5182a04f96ae
Diogo Alves <diogocalves26@gmail.com>
2024-12-13 01:15:20 +0000

    Initialized the MenuState class

--- src/main/java/controller/state/MenuState.java
+++ src/main/java/controller/state/MenuState.java
@@ -1,4 +1,14 @@
 package controller.state;
 
+import com.googlecode.lanterna.screen.Screen;
+import model.game.Game;
+import view.menu.MenuViewer;
+
 public class MenuState {
+    private final MenuViewer menuViewer;
+
+    public MenuState(Screen screen, Game game) {
+        this.menuViewer = new MenuViewer(game.getMenuModel(), screen); // need to implement
+                                                                      // a getMenuModel() on the game class
+    }
 }
