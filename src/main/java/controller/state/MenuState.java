ea64a33152e67ef84e397779b613a631693b150d
Diogo Alves <diogocalves26@gmail.com>
2024-12-13 02:22:25 +0000

    Some minor updated

--- src/main/java/controller/state/MenuState.java
+++ src/main/java/controller/state/MenuState.java
@@ -1,14 +1,22 @@
 package controller.state;
 
 import com.googlecode.lanterna.screen.Screen;
+import model.Setup;
 import model.game.Game;
 import model.menu.MenuModel;
+import view.Viewer;
 import view.menu.MenuViewer;
 
-public class MenuState {
+public class MenuState  extends ControllerState<MenuModel>{
 
-    public MenuState(Screen screen) {
+    public MenuState(Setup setup) {
+        super(setup);
 
+    }
+
+    @Override
+    public Viewer<MenuModel> getViewer() {
+        return null;    // for now
     }
 
 }
