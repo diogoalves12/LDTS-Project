1010c151d16093520de75bd7f1f27ff2388012fd
Diogo Alves <diogocalves26@gmail.com>
2024-12-13 02:23:04 +0000

    Added the ControllerState class

--- src/main/java/controller/state/ControllerState.java
+++ src/main/java/controller/state/ControllerState.java
@@ -1,5 +1,29 @@
 package controller.state;
 
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
+import model.Setup;
+import view.Viewer;
+
+import java.io.IOException;
+
 public abstract class ControllerState<T> {
+    Setup setup;
+    TextGraphics textGraphics;
+
+    public ControllerState(Setup setup){
+        this.setup = setup;
+    }
+
+    public void changeState(ControllerState<T> nextState){
+
+    }
+
+    public abstract Viewer<T> getViewer();
+
+    public void draw(TextGraphics textGraphics) throws IOException {
+        this.getViewer().draw();
+    }
+
 
 }
