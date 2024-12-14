f2f1cd735b2595f82cabcbde69f079428ba33423
Diogo Alves <diogocalves26@gmail.com>
2024-12-14 23:45:35 +0000

    Add constructor

--- src/main/java/control/state/ControllerState.java
+++ src/main/java/control/state/ControllerState.java
@@ -1,5 +1,14 @@
 package control.state;
 
+import model.GameSetup;
+
 public class ControllerState<T> {
+    GameSetup setup;
+    StateFactory factory;
+
+    public ControllerState(GameSetup setup, StateFactory factory) {
+        this.setup = setup;
+        this.factory = factory;
+    }
 
 }
