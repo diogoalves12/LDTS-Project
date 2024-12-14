01b675885c1913bd0833e41ae3456d074604a17d
Diogo Alves <diogocalves26@gmail.com>
2024-12-14 23:46:29 +0000

    Added the MenuModel to the factory

--- src/main/java/control/state/StateFactory.java
+++ src/main/java/control/state/StateFactory.java
@@ -1,6 +1,15 @@
 package control.state;
 
+import model.GameSetup;
+import model.menu.MenuModel;
+import view.menu.MenuView;
+
 public class StateFactory {
 
-    public MenuState
+    public MenuState getMenuState(GameSetup setup) {
+        MenuModel model = new MenuModel();
+        MenuView view = new MenuView(model);
+        return new MenuState(model, view, setup, this);
+    }
+
 }
