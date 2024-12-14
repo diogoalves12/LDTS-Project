d9ba653f1d73a871a3468b3e82f20e0390c998ab
Diogo Alves <diogocalves26@gmail.com>
2024-12-14 16:42:25 +0000

    Added some code to help state

--- src/main/java/controller/state/HelpState.java
+++ src/main/java/controller/state/HelpState.java
@@ -1,8 +1,12 @@
 package controller.state;
 
+import com.googlecode.lanterna.input.KeyStroke;
 import model.help.HelpModel;
 import view.View;
 import view.help.HelpPageView;
+
+import javax.swing.*;
+import java.io.IOException;
 
 public class HelpState extends ControllerState<HelpModel> {
     private final HelpModel model;
@@ -16,11 +20,26 @@ public class HelpState extends ControllerState<HelpModel> {
 
     @Override
     public View<HelpModel> getViewer() {
-        return null;
+        return view;
     }
 
     @Override
     public ControllerState<?> changeState() {
-        return null;
+        try {
+            view.draw();
+            KeyStroke key = view.getScreen().pollInput();
+            if(key != null) {
+                switch (key.getKeyType()) {
+                    case Escape:
+                        new MenuState(null);
+                    default:
+                }
+            }
+            return this;
+
+        } catch (IOException e) {
+           e.printStackTrace();
+           return null;
+        }
     }
 }
