61a7af108ce828a8aec270ab35596470c8e0d464
Diogo Alves <diogocalves26@gmail.com>
2024-12-14 17:53:30 +0000

    Added more cases to the state factory

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,13 +1,17 @@
 package controller;
 
 import controller.state.ControllerState;
+import controller.state.FactoryState;
+import model.Setup;
+
+import javax.naming.spi.StateFactory;
 
 public class Controller {
     private ControllerState<?> state;
 
-
-    public Controller() {
-
+    public Controller(FactoryState Factory) {
+        Setup setup = new Setup(30,30, Setup.Difficulty.NORMAL);
+        this.state = Factory.createState("MENU", setup);
     }
 
 }
--- src/main/java/controller/state/FactoryState.java
+++ src/main/java/controller/state/FactoryState.java
@@ -7,6 +7,8 @@ public class FactoryState {
     public static ControllerState<?> createState(String stateName, Setup setup) {
         return switch (stateName) {
             case "MENU" -> new MenuState(setup);
+            case "GAME" -> new GameState(setup);
+            case "HELP" -> new HelpState();
             default -> throw new IllegalStateException("Unexpected value: " + stateName);
         };
     }
