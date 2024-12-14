491f58b617ee740cd906332a09d957cd5e0bfd53
Diogo Alves <diogocalves26@gmail.com>
2024-12-14 23:00:38 +0000

    Restructure game class

--- src/main/java/model/game/Game.java
+++ src/main/java/model/game/Game.java
@@ -4,33 +4,19 @@ import model.GameSetup;
 
 public class Game {
 
-    private static final int EASY_FREQUENCY = 8;
-    private static final int NORMAL_FREQUENCY = 6;
-    private static final int HARD_FREQUENCY = 5;
-
     private final Board board;
     private final GameSetup setup;
 
     public Game(GameSetup setup) {
         this.setup = setup;
-        this.board = initBoard();
+        this.board = initializeBoard();
     }
 
-    private Board initBoard(){
-        int freq = calculateMineFrequency();
+    private Board initializeBoard() {
         Board.initialize(setup.getBoardRows(), setup.getBoardCols());
         BoardBuilder builder = new BoardBuilder();
-        builder.buildBoard(freq);
+        builder.buildBoard(setup.getMineFrequency());
         return Board.getInstance();
-    }
-
-    private int calculateMineFrequency(){
-        return switch (setup.getDifficulty()) {
-            case EASY -> 8;
-            case NORMAL -> 6;
-            case HARD -> 5;
-            default -> throw new IllegalArgumentException("Invalid difficulty");
-        };
     }
 
     public Board getBoard() {
