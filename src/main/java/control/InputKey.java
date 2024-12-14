4799b8c2d229acec96ab82d830e8670a5172c344
Diogo Alves <diogocalves26@gmail.com>
2024-12-14 23:57:26 +0000

    Add constructure and input types

--- src/main/java/control/InputKey.java
+++ src/main/java/control/InputKey.java
@@ -1,4 +1,25 @@
 package control;
 
 public class InputKey {
+    public enum  INPUT {
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT,
+        ENTER,
+        ESCAPE,
+        FLAG,
+        NONE,
+    }
+
+    INPUT input;
+    Character key;
+
+    public InputKey() {
+        this.input = INPUT.NONE;
+        this.key = ' ';
+    }
+
+
+
 }
