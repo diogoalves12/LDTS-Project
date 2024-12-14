8e56a63032b62faab00b35784a3d06f2856864fc
Diogo Alves <diogocalves26@gmail.com>
2024-12-14 00:30:04 +0000

    Added some code to help page

--- src/main/java/view/help/HelpPageView.java
+++ src/main/java/view/help/HelpPageView.java
@@ -6,5 +6,21 @@ import view.View;
 import java.io.IOException;
 
 public class HelpPageView extends View<HelpModel> {
+    private static final String DEFAULT_TITLE = "HELP";
+
+    public HelpPageView(HelpModel model) {
+        super(model);
+    }
+
+    @Override
+    public void draw() throws IOException {
+        clear();
+
+        // yet to implement
+        drawTitle();
+        drawContent();
+        refresh();
+
+    }
 
 }
