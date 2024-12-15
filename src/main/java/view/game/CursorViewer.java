3e689645ec294d4962ac44b961367ce95638e91d
Diogo Alves <diogocalves26@gmail.com>
2024-12-15 03:20:49 +0000

    Created the cursor viewer

---
+++ src/main/java/view/game/CursorViewer.java
@@ -0,0 +1,30 @@
+package view.game;
+
+import com.googlecode.lanterna.TextColor;
+import model.game.Cursor;
+import view.View;
+
+import java.io.IOException;
+
+public class CursorViewer extends View<Cursor> {
+
+    public CursorViewer(Cursor model) {
+        super(model);
+    }
+
+    @Override
+    public void draw() throws IOException {
+        graphics.setForegroundColor(TextColor.Factory.fromString("#F000FF"));
+
+        int row = getModel().getRow();
+        int col = getModel().getCol();
+
+        graphics.putString(col * 2, row, "[");
+        graphics.putString(col * 2 + 2, row, "]");
+        graphics.setForegroundColor(TextColor.Factory.fromString("#000000"));
+
+        refresh();
+    }
+
+
+}
