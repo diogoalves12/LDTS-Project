4e33f2ae6e7baa7fa422985afd26db6ec315b3cb
Diogo Alves <diogocalves26@gmail.com>
2024-12-15 00:19:09 +0000

    finish input key class

--- src/main/java/control/InputKey.java
+++ src/main/java/control/InputKey.java
@@ -1,25 +1,57 @@
 package control;
 
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.screen.Screen;
+
+import java.io.IOException;
+
 public class InputKey {
-    public enum  INPUT {
+
+    public enum INPUT {
         UP,
         DOWN,
         LEFT,
         RIGHT,
         ENTER,
-        ESCAPE,
         FLAG,
+        ESCAPE,
         NONE,
+        QUIT,
     }
 
-    INPUT input;
-    Character key;
+    private INPUT input;
+    private Character key;
 
     public InputKey() {
         this.input = INPUT.NONE;
         this.key = ' ';
     }
 
+    public INPUT getInput() {
+        return input;
+    }
 
+    public Character getKey() {
+        return key;
+    }
+
+    public InputKey processInput(Screen screen) throws IOException {
+        KeyStroke keyStroke = screen.pollInput();
+        if (keyStroke == null) {
+            return this;
+        }
+
+        InputMapper.mapKeyStroke(this, keyStroke);
+        return this;
+    }
+
+    public void setInput(INPUT input) {
+        this.input = input;
+    }
+
+    public void setKey(Character key) {
+        this.key = key;
+    }
 
 }
+
