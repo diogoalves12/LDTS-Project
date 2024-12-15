e9d4356fb8addec6103134fecf177fc78702e711
Diogo Alves <diogocalves26@gmail.com>
2024-12-15 03:24:07 +0000

    Add cursor viewer to the factory

--- src/main/java/view/ViewFactory.java
+++ src/main/java/view/ViewFactory.java
@@ -1,9 +1,11 @@
 package view;
 
 import model.game.Board;
+import model.game.Cursor;
 import model.help.HelpModel;
 import model.menu.MenuModel;
 import view.game.BoardViewer;
+import view.game.CursorViewer;
 import view.help.HelpPageView;
 import view.menu.MenuView;
 
@@ -19,6 +21,10 @@ public class ViewFactory {
 
     public static HelpPageView createHelpPageView(HelpModel model)  {
         return new HelpPageView(model);
+    }
+
+    public static CursorViewer createCursorView(Cursor cursor) {
+        return new CursorViewer(cursor);
     }
 
 }
