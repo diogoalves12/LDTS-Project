5d98a0e99316a71610090dd79a64fd26135ee230
Diogo Alves <diogocalves26@gmail.com>
2024-12-15 04:04:58 +0000

    created an main method to run the game

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,13 @@
+import control.Controller;
+import control.state.StateFactory;
+
 public class Main {
     public static void main(String[] args) {
+        Controller controller = new Controller(new StateFactory());
+        try {
+            controller.launch();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
