797fbbef4bec25d8e39cae75e7c04d39e1b13502
Diogo Alves <diogocalves26@gmail.com>
2024-12-15 01:56:59 +0000

    add the game state to the factory

--- src/main/java/control/state/StateFactory.java
+++ src/main/java/control/state/StateFactory.java
@@ -1,15 +1,26 @@
 package control.state;
 
 import model.GameSetup;
+import model.game.Game;
 import model.menu.MenuModel;
+import view.ViewFactory;
+import view.game.BoardViewer;
 import view.menu.MenuView;
 
 public class StateFactory {
 
     public MenuState getMenuState(GameSetup setup) {
         MenuModel model = new MenuModel();
-        MenuView view = new MenuView(model);
+        MenuView view = ViewFactory.createMenuView(model);
         return new MenuState(model, view, setup, this);
     }
+
+    public GameState getGameState(GameSetup setup) {
+        Game game = new Game(setup);
+        BoardViewer viewer = ViewFactory.createBoardViewer(game.getBoard());
+        return new GameState(game, viewer, setup, this);
+    }
+
+
 
 }
