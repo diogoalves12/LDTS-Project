0d3ab0bce5e6e627720588a333bd817637087e62
Diogo Alves <diogocalves26@gmail.com>
2024-12-15 01:01:38 +0000

    Add constructor and needed variables and methods

--- src/main/java/control/state/MenuState.java
+++ src/main/java/control/state/MenuState.java
@@ -1,13 +1,31 @@
 package control.state;
 
+import com.googlecode.lanterna.graphics.TextGraphics;
+import control.Controller;
+import control.InputKey;
 import model.GameSetup;
 import model.menu.MenuModel;
+import view.View;
 import view.menu.MenuView;
 
-public class MenuState extends ControllerState<MenuModel>{
-
+public class MenuState extends ControllerState<MenuModel> {
+    TextGraphics graphics;
+    MenuModel model;
+    MenuView view;
 
     public MenuState(MenuModel model, MenuView view, GameSetup setup, StateFactory factory) {
+        super(setup, factory);
+        this.model = model;
+        this.view = view;
+        this.graphics = view.getGraphics();
+    }
+
+
+    @Override
+    public View<MenuModel> getViewer() { return view; }
+
+    @Override
+    public void update(Controller controller, InputKey inputkey) {
 
     }
 }
