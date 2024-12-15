0cf6fce7a6fa168b26cd0b2d869d3f1ee59cea06
Diogo Alves <diogocalves26@gmail.com>
2024-12-15 01:56:34 +0000

    Add the constructor and needed methods

--- src/main/java/control/state/HelpState.java
+++ src/main/java/control/state/HelpState.java
@@ -1,4 +1,31 @@
 package control.state;
 
-public class HelpState {
+import control.Controller;
+import control.InputKey;
+import model.GameSetup;
+import model.help.HelpModel;
+import view.View;
+import view.help.HelpPageView;
+
+import java.io.IOException;
+
+public class HelpState extends ControllerState<HelpModel> {
+    HelpModel model;
+    HelpPageView viewer;
+
+    public HelpState(HelpModel model, HelpPageView viewer, GameSetup setup, StateFactory factory) {
+        super(setup, factory);
+        this.model = model;
+        this.viewer = viewer;
+    }
+
+    @Override
+    public View<HelpModel> getViewer() {
+        return null;
+    }
+
+    @Override
+    public ControllerState<?> update(Controller controller, InputKey inputkey) throws IOException {
+        return null;
+    }
 }
