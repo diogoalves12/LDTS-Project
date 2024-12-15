d07ce1d1d2ab7c3da5fad512f11758c5084d9e2b
Diogo Alves <diogocalves26@gmail.com>
2024-12-15 01:00:34 +0000

    Add the code needed to the abstract Controller state class

--- src/main/java/control/state/ControllerState.java
+++ src/main/java/control/state/ControllerState.java
@@ -1,14 +1,41 @@
 package control.state;
 
+import com.googlecode.lanterna.graphics.TextGraphics;
+import control.Controller;
+import control.InputKey;
 import model.GameSetup;
+import view.View;
 
-public class ControllerState<T> {
-    GameSetup setup;
+import java.io.IOException;
+
+public abstract class ControllerState<T> {
+
     StateFactory factory;
+    GameSetup setup;
+
 
     public ControllerState(GameSetup setup, StateFactory factory) {
         this.setup = setup;
         this.factory = factory;
     }
 
+    public abstract View<T> getViewer();
+
+    protected void draw() throws IOException {
+        this.getViewer().draw();
+    }
+
+    protected ControllerState<T> changeState(ControllerState<T> nextState) throws IOException {
+        if(nextState != this) this.getViewer().close();
+        return nextState;
+    }
+
+    public InputKey processInput() throws IOException {
+        draw();
+        InputKey inputKey = this.getViewer().getCommand();
+        if(inputKey == null) return this.getViewer().getCommand();
+        return inputKey;
+    }
+
+    public abstract void update(Controller controller, InputKey inputkey);
 }
