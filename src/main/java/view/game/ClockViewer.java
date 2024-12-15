310a7fb38307b8213dd7247e42ebf4a6b5a71cef
Diogo Alves <diogocalves26@gmail.com>
2024-12-15 04:47:54 +0000

    Add the logic behind clock viewer

--- src/main/java/view/game/ClockViewer.java
+++ src/main/java/view/game/ClockViewer.java
@@ -1,5 +1,7 @@
 package view.game;
 
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.graphics.TextGraphics;
 import model.game.GameClock;
 import view.View;
 
@@ -12,7 +14,37 @@ public class ClockViewer extends View<GameClock> {
 
     @Override
     public void draw() throws IOException {
+        TextGraphics graphics = getGraphics();
+        clear();
 
+        GameClock model = getModel();
+        String time = model.getElapsedSeconds() + "s";
+
+        TerminalSize size = getSize();
+
+        graphics.putString(size.getColumns() / 2 - "Time:".length() / 2, 2, "Time");
+
+        drawBorder();
+
+        graphics.putString(size.getColumns() / 2 - time.length() / 2, size.getRows() / 2, time);
+
+        refresh();
 
     }
+
+    private void drawBorder(){
+        TextGraphics graphics = getGraphics();
+        TerminalSize size = getSize();
+
+        for(int col = 0; col < size.getColumns(); col++){
+            graphics.putString(col, 1, "-");
+            graphics.putString(col, size.getRows() - 2, "-");
+        }
+
+        for(int row = 0; row < size.getRows() - 2 ; row++){
+            graphics.putString(0, row, "|");
+            graphics.putString(size.getColumns() - 1, row, "|");
+        }
+    }
+
 }
