ddeb38c4ec4db34e5d4bf216616f6e01a24f0a0b
Diogo Alves <diogocalves26@gmail.com>
2024-12-16 00:57:13 +0000

    Create the model of Game Over

--- src/main/java/model/game/GameOver.java
+++ src/main/java/model/game/GameOver.java
@@ -1,4 +1,38 @@
 package model.game;
 
+import java.util.ArrayList;
+
 public class GameOver {
+    private ArrayList<String> text;
+
+
+    public GameOver(){
+        this.text = new ArrayList<>();
+        loadContent();
+    }
+
+    private void loadContent(){
+        text.add("Game Over!");
+        text.add("You lost!");
+        text.add("Better luck next time!");
+        text.add("Press 'ESC' to the menu.");
+    }
+
+    public ArrayList<String> text(){
+        return text;
+    }
+
+    public String getSection(int index){
+        if(index >= 0 && index < text.size()){
+            return text.get(index);
+        }else{
+            throw new IllegalArgumentException("Índice inválido: " + index);
+        }
+    }
+
+    public int getTotalSections(){
+        return text.size();
+    }
+
 }
+
