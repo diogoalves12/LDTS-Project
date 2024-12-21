819c7d92cdbaf1ea578bd21e0eb9e7dbd7c6b5fe
Diogo Alves <diogocalves26@gmail.com>
2024-12-21 19:50:46 +0000

    Add the base test

--- src/test/java/model/menu/MenuModelTest.java
+++ src/test/java/model/menu/MenuModelTest.java
@@ -1,4 +1,16 @@
 package model.menu;
 
+import model.GameSetup;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MenuModelTest {
+
+    @Test
+    public void testMenuModel() {
+        MenuModel model = new MenuModel();
+        assertEquals( 0, model.getSelected());
+        assertEquals(GameSetup.Difficulty.NORMAL, model.getDifficulty());
+        assertEquals(3, model.getOptions().size());
+    }
 }
