03b682a2393c4c8478523e8992243f0b2bf8ce8c
Diogo Alves <diogocalves26@gmail.com>
2024-12-21 19:40:51 +0000

    Restructure of the old tests and add  more tests

--- src/test/java/model/GameSetupTest.java
+++ src/test/java/model/GameSetupTest.java
@@ -7,21 +7,36 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class GameSetupTest {
 
     @Test
-    public void testGetMineFrequencyEasy() {
-        GameSetup gameSetup = new GameSetup(GameSetup.Difficulty.EASY);
-        assertEquals(8, gameSetup.getMineFrequency());
+    public void testBoardSetup() {
+        GameSetup setup = new GameSetup(20,20,5,GameSetup.Difficulty.HARD);
+        assertEquals(20, setup.getBoardRows());
+        assertEquals(20, setup.getBoardCols());
+        assertEquals(5, setup.getMineFrequency());
+        assertEquals(GameSetup.Difficulty.HARD, setup.getDifficulty());
     }
 
     @Test
-    public void testGetMineFrequencyNormal() {
-        GameSetup gameSetup = new GameSetup(GameSetup.Difficulty.NORMAL);
-        assertEquals(6, gameSetup.getMineFrequency());
+    public void testEasyDifficulty() {
+        GameSetup setup = new GameSetup(GameSetup.Difficulty.EASY);
+        assertEquals(8, setup.getBoardRows());
+        assertEquals(8, setup.getBoardCols());
+        assertEquals(8, setup.getMineFrequency());
     }
 
     @Test
-    public void testGetMineFrequencyHard() {
-        GameSetup gameSetup = new GameSetup(GameSetup.Difficulty.HARD);
-        assertEquals(5, gameSetup.getMineFrequency());
+    public void testNormalDifficulty() {
+        GameSetup setup = new GameSetup(GameSetup.Difficulty.NORMAL);
+        assertEquals(14, setup.getBoardRows());
+        assertEquals(14, setup.getBoardCols());
+        assertEquals(6, setup.getMineFrequency());
+    }
+
+    @Test
+    public void testHardDifficulty() {
+        GameSetup setup = new GameSetup(GameSetup.Difficulty.HARD);
+        assertEquals(20, setup.getBoardRows());
+        assertEquals(20, setup.getBoardCols());
+        assertEquals(5, setup.getMineFrequency());
     }
 
 }
