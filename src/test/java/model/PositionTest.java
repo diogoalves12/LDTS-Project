4e1ba116f60143deaf952223d53eb2869e6274a9
Diogo Alves <diogocalves26@gmail.com>
2024-12-21 19:19:20 +0000

    Add tests to PositionTest class

--- src/test/java/model/PositionTest.java
+++ src/test/java/model/PositionTest.java
@@ -1,3 +1,22 @@
+package model;
+
+import net.jqwik.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class PositionTest {
+
+    @Property
+    public void testValidPosition(@ForAll int row, @ForAll int col) {
+        Position pos = new Position(row, col);
+        assertEquals(row, pos.getRow());
+        assertEquals(col, pos.getCol());
+    }
+
+    @Property
+    public void testToStringMethod(@ForAll int row, @ForAll int col) {
+        Position pos = new Position(row, col);
+        assertEquals("(" + row + "," + col+ ")", pos.toString());
+    }
 
 }
