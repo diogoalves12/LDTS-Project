600ef549e8e713c1efef40d0cf377eeeada79533
Diogo Alves <diogocalves26@gmail.com>
2024-12-21 19:26:00 +0000

    Add some tests that test getMineFrequency

--- src/test/java/model/GameSetupTest.java
+++ src/test/java/model/GameSetupTest.java
@@ -1,4 +1,27 @@
 package model;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class GameSetupTest {
+
+    @Test
+    public void testGetMineFrequencyEasy() {
+        GameSetup gameSetup = new GameSetup(GameSetup.Difficulty.EASY);
+        assertEquals(8, gameSetup.getMineFrequency());
+    }
+
+    @Test
+    public void testGetMineFrequencyNormal() {
+        GameSetup gameSetup = new GameSetup(GameSetup.Difficulty.NORMAL);
+        assertEquals(6, gameSetup.getMineFrequency());
+    }
+
+    @Test
+    public void testGetMineFrequencyHard() {
+        GameSetup gameSetup = new GameSetup(GameSetup.Difficulty.HARD);
+        assertEquals(5, gameSetup.getMineFrequency());
+    }
+
 }
